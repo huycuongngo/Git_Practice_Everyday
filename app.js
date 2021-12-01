1e85f2315ee2fb74df3b8245464e24658156e11a
Luffy <cuongxuyentu99@gmail.com>
2021-12-01 21:28:28 +0700

    master-C2

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 
-const logger = (...params) => {
-    console.log(params);
+const logger = (a, b, ...params) => {
+  console.log(a, b);
+  console.log(params);
 }
 
 logger(1, 2, 3, 4);
+
+const myObject = ({ name, price, description, ...parmas1 }) => {
+  console.log(name);
+  console.log(price);
+  console.log(description);
+  console.log(parmas1);
+}
+myObject({
+  name: 'Javascript',
+  price: 300,
+  description: `description content`,
+  ID: 123,
+  Room: `101H3`,
+});
+
